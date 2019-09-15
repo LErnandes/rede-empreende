e5d86988c7140c8cbe5e4100eb78cab500e086dc
Moises da China <luis78270@gmail.com>
2019-09-14 21:41:20 -0300

    at

--- src/store.js
+++ src/store.js
@@ -5,7 +5,10 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    posts: [
+      {id: 0, btns: {state: 0, color: ''}, author: {name: 'David K. Downs', img: 'https://avatars0.githubusercontent.com/u/9064066?v=4&s=460'}, img: 'https://i.zst.com.br/images/as-4-melhores-cadeiras-para-escritorio-em-2019-photo745722425-44-7-37.jpg', text: 'Ipsum, cacilds vidis litro abertis.'}, 
+      {id: 1, btns: {state: 0, color: ''}, author: {name: 'David K. Downs', img: 'https://avatars0.githubusercontent.com/u/9064066?v=4&s=460'}, img: 'https://i.zst.com.br/images/as-4-melhores-cadeiras-para-escritorio-em-2019-photo745722425-44-7-37.jpg', text: 'Ipsum, cacilds vidis litro abertis.'},
+    ]
   },
   mutations: {
 
--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,28 +1,56 @@
 <template>
     <v-app>
         <v-container>
-            <v-row justify="center">
+            <v-row v-bind:key="post.id" v-for="post in posts" justify="center">
                 <v-col cols="6">
                     <v-card>
                         <v-list-item>
                             <v-list-item-avatar size="55" color="grey">
-                                <v-img src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"></v-img>
+                                <v-img :src="post.author.img"></v-img>
                             </v-list-item-avatar>
                             <v-list-item-content>
-                                <v-list-item-title>David K. Downs</v-list-item-title>
+                                <v-list-item-title>{{ post.author.name }}</v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
 
                         <v-card-text>
                             <span class="text--primary">
-                                <span>Ipsum, cacilds vidis litro abertis.</span><br>
-                                <span>Manduma pindureta quium dia nois paga. Per aumento de cachacis, eu reclamis. Copo furadis é disculpa de bebadis, arcu quam euismod magna. A ordem dos tratores não altera o pão duris.</span>
+                                {{ post.text }}
                             </span>
                         </v-card-text>
-                        <v-img src=""></v-img>
+
+                        <v-img :src="post.img"></v-img>
+
+                        <v-row justify="end">
+                            <v-col cols="2">
+                                <v-btn :color="post.btns.color" @click="post.btns.state ^= true; if (post.btns.state === 1) {post.btns.color = 'pink'} else {post.btns.color = ''}" icon>
+                                    <v-icon>mdi-heart</v-icon>
+                                </v-btn>
+                                <v-btn icon>
+                                    <v-icon>mdi-share-variant</v-icon>
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+
                     </v-card>
                 </v-col>
             </v-row>
         </v-container>
     </v-app>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+    computed: {
+        ... mapState({
+            posts: state => state.posts
+        })
+    },
+    data: () => {
+        return {
+            post: {},
+        }
+    }
+}
+</script>
